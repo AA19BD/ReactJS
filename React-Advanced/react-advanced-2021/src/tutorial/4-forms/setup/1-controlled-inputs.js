55bf0ce3fb7de131a838f81acac4e3a05cc96ca6
Abilay Aytbanov <aitbanov640@gmail.com>
2021-11-14 23:37:50 +0600

    Forms-dynamic object keys

--- React-Advanced/react-advanced-2021/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ React-Advanced/react-advanced-2021/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,51 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const[firstName,setFirstName]=useState('')
+  const[email,setEmail]=useState('')
+  const[people,setPeople]=useState([])
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(firstName && email){
+      const person={id:new Date().getTime().toString(),firstName,email}
+      setPeople((people)=>{
+        return [...people,person]
+      })
+    }
+    setFirstName('')
+    setEmail('')
+  }
+
+  return (
+    <React.Fragment>
+    <article>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="firsrtName">Name : </label>
+          <input type="text" id="firstName" name="firstName"  required value={firstName} onChange={(e)=>setFirstName(e.target.value)} /> 
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email : </label>
+          <input type="email" id="email" name="email" required value={email} onChange={(e)=>setEmail(e.target.value)}/> 
+        </div>
+        <button type="submit">Add Person</button>
+      </form>
+      {people.map((person)=>{
+        const {id,firstName,email}=person
+        return(
+          <div className="item" key={id}> 
+            <h4>{firstName}</h4>
+            <p>{email}</p>
+          </div>
+        )
+      })}
+   </article>
+   
+    </React.Fragment>
+  
+  )
+  
 };
 
 export default ControlledInputs;
