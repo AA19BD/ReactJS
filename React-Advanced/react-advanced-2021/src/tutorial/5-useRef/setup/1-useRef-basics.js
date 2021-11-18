90223f1dc5d0a152bc77ebf681d2dbc3b2b45e0d
Abilay Aytbanov <aitbanov640@gmail.com>
2021-11-19 00:37:15 +0600

    useRef-hook-preseve the value for full lifetime

--- React-Advanced/react-advanced-2021/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ React-Advanced/react-advanced-2021/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef,useState } from 'react';
 
 // preserves value
-// DOES NOT trigger re-render
-// target DOM nodes/elements
+//useRef==works as useState but-> // 1)DOES NOT trigger re-render
+//target DOM nodes/elements
 
+/*useState should be used when the value of state should affect what gets rendered. 
+useRef should be used when you want to have a piece of information that persists “for the full lifetime of the component” - not just during its render cycle */
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer=useRef(null)
+  const divContainer=useRef(null)
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(refContainer.current.value) 
+    console.log(divContainer.current)
+  }
+  // console.log(refContainer);
+  useEffect(()=>{
+    console.log(refContainer.current)
+    refContainer.current.focus()//like->useEffect(()=>{},[.focus()])
+
+  })
+  return <>
+  <form className="form" onSubmit={handleSubmit}>
+      <div>
+        <input type="text" ref={refContainer}/>
+        <button type='submit'>Submit</button>
+      </div>
+  </form>
+  <div ref={divContainer}>Hello World!</div>
+  </>;
 };
 
 export default UseRefBasics;
