a007acb5f306500f72156b105f2d38a719de3514
Abilay Aytbanov <aitbanov640@gmail.com>
2021-12-08 02:59:49 +0600

    React-2021-useReducer-prop-drilling

---
+++ React-Advanced/react-advanced-2021/src/tutorial/6-useReducer/setup/reducer.js
@@ -0,0 +1,37 @@
+export const reducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "Item added!",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "EMPTY INPUT!",
+    };
+  }
+  if (action.type === "CLOSE_MODAL") {
+    return {
+      ...state,
+      isModalOpen: false,
+    };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const newPeople = state.people.filter(
+      (person) => person.id !== action.payload
+    );
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: "REMOVED!",
+    };
+  }
+  throw new Error("NO MATCHING ACTION TYPE");
+  // return state;
+};
